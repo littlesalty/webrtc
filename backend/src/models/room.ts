d35374653dda47d959f36ad9f02a03f161102a67
ascariu <26717750+ascariu@users.noreply.github.com>
2021-10-01 19:42:10 +0200

    added room model

---
+++ backend/src/models/room.ts
@@ -0,0 +1,28 @@
+export type ChatRoom = {
+    name: string
+    uuid: string
+}
+
+
+export const mockRooms: Array<ChatRoom> = [
+{
+    name: "potato",
+    uuid:  "4a8239af-43f4-4a19-ae80-11eb82d1b078"
+},
+{
+    name: "potato",
+    uuid:  "d1466815-aa63-4de2-823b-20fac1da471d"
+},
+{
+    name: "potato",
+    uuid:  "9addebb9-0f6e-4280-a72f-406c50d604cc"
+},
+{
+    name: "potato",
+    uuid:  "ea379435-5a6b-4a84-96d2-8b8d99f230d7"
+},
+{
+    name: "potato",
+    uuid:  "ecc70769-c57c-4f3b-a9df-bd4eb1348a87"
+},
+]
