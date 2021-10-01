90fb1c1e50690a994511877c138a91c6cf7088b6
ascariu <26717750+ascariu@users.noreply.github.com>
2021-10-01 19:55:18 +0200

    handle socket connection on init

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,6 +22,7 @@ export class Server {
     this.app = express()
     this.httpServer = createServer(this.app)
     this.io = new SocketIOServer(this.httpServer)
+    this.handleSocketConnection()
   }
 
   private handleRoutes(): void {
