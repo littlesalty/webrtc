6261689a94fae8418a06305657a4e08587dfeac2
Miquel Àngel Román <miquelrc95@outlook.es>
2021-10-02 13:25:51 +0200

    Adds prod env configurations

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { InjectionToken, NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { UiModule } from '../ui/ui.module';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 export const HOST = new InjectionToken<string>('Http host')
 export const WS_BASE_URL = new InjectionToken<string>('WebSocket URL');
 export const HTTP_BASE_URL = new InjectionToken<string>('Http URL');
 
 
-const hostName = 'localhost:4200'
+const hostName = environment.hostName
 
 @NgModule({
   imports: [
