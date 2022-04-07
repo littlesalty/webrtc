ea35320d9fead568e23b9dee6f03c5a5def2eeaf
Miquel Àngel Román <miquelrc95@outlook.es>
2022-04-07 23:52:10 +0200

    Make environment handling automatic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,17 +4,8 @@ import { Server } from "./server"
 export type ConfigFile = {
     production: boolean
 }
-const configLoader = (): ConfigFile => {
-
-    const fileToLoad = process.env.NODE_ENV === "prod" ?
-        "./environment/prod.json" : "./environment/dev.json"
-
-    return require(fileToLoad)
-}
-
+const configLoader = (): ConfigFile => require('./environment/env.json')
 const config = configLoader()
 const server = new Server(config)
 
-server.listen(port => {
-    console.log(`Server is listening on http://localhost:${port}/chat-history`)
-})
+server.listen(port => console.log(`Server running on http://localhost:${port}`))
